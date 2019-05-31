e1a7307faca057fb32482e787189fa4ebfdf6e4f
Josh Teperman <joshteperman@gmail.com>
2019-05-31 16:27:14 +1000

    WIP, cant remember

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-const run = () => {
-  getTestData();
-} 
+// import React from 'react'
+// import './App.css'
+// import { fontawesomeIcon } from '@fontawesome/react-fontawesome'
+// const axios = require('axios')
+// const API_ENDPOINT = 'url'
 
-async function getTestData() {
-  const testData = await axios.get('url')
-  console.log(testData)   
-}
+const express = require('express')
+
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+// const run = () => {
+//   getTestData();
+// } 
+
+// async function getTestData() {
+//   const testData = await axios.get('url')
+//   console.log(testData)   
+// }
